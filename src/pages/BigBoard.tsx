bfd377a73a239a65d4e2033479a7e8bcf9ae9e37
BMayenschein <brucedmayenschein@gmail.com>
2024-08-03 13:19:49 -0400

    initial API setup to grab players from mongoDB

--- src/pages/BigBoard.tsx
+++ src/pages/BigBoard.tsx
@@ -1,5 +1,17 @@
-export default function BigBoard() {
+import { useState } from "react"
+
+const BigBoard = () => {
+    const [players, setPlayers] = useState("")
+    const getPlayers =  async () => {
+        const res = await fetch("http://localhost:5000/players/getPlayers")
+        const data = await res.json();
+    
+        setPlayers(data)
+    }
+
     return (
         <h1>Big Board</h1>
     )
 }
+
+export default BigBoard;
