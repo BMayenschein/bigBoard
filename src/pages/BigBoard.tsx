048bdbd741dd4ef0fbb56d028b701a3878fac0a0
BMayenschein <brucedmayenschein@gmail.com>
2024-08-03 21:28:39 -0400

    updated API call using useEffect()

--- src/pages/BigBoard.tsx
+++ src/pages/BigBoard.tsx
@@ -1,16 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import PlayerCard from "../components/PlayerCard"
+
+type PlayerInfo = {
+    id: string,
+    firstName: string,
+    lastName: string,
+    position: string,
+    prevTeam: string,
+    age: string,
+    height: string,
+    wingspan: string,
+    PTS: string,
+    REB: string,
+    AST: string
+}
 
 const BigBoard = () => {
-    const [players, setPlayers] = useState("")
-    const getPlayers =  async () => {
-        const res = await fetch("http://localhost:5000/players/getPlayers")
-        const data = await res.json();
-    
-        setPlayers(data)
-    }
+    const [players, setPlayers] = useState<PlayerInfo[]>([])
+    // const getPlayers =  async () => {
+    //     try {
+    //         const res = await fetch("http://localhost:5000/players/getPlayers")
+    //         const data = await res.json()
+        
+    //         setPlayers(data)
+    //     }
+    //     catch(err) {
+    //         console.log(err)
+    //     }
+    // }
+
+    useEffect(() => {
+        async function getPlayers() {
+            const res = await fetch("http://localhost:5000/players/getPlayers")
+            const data = await res.json()
+            setPlayers(data)
+            console.log(players)
+        }
+        getPlayers()
+    }, [])
 
     return (
-        <h1>Big Board</h1>
+        <>
+            {players.map((player, index) => {
+                <p key={index}>{player.firstName}</p>
+            })}
+        </>
     )
 }
 
