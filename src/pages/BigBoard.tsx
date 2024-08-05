53db8c9e9d14e4afec89c47b0969d096821d3164
BMayenschein <brucedmayenschein@gmail.com>
2024-08-05 12:37:24 -0400

    removed old commented code. updated render to display new PlayerCard component

--- src/pages/BigBoard.tsx
+++ src/pages/BigBoard.tsx
@@ -17,33 +17,28 @@ type PlayerInfo = {
 
 const BigBoard = () => {
     const [players, setPlayers] = useState<PlayerInfo[]>([])
-    // const getPlayers =  async () => {
-    //     try {
-    //         const res = await fetch("http://localhost:5000/players/getPlayers")
-    //         const data = await res.json()
-        
-    //         setPlayers(data)
-    //     }
-    //     catch(err) {
-    //         console.log(err)
-    //     }
-    // }
 
     useEffect(() => {
         async function getPlayers() {
             const res = await fetch("http://localhost:5000/players/getPlayers")
             const data = await res.json()
             setPlayers(data)
-            console.log(players)
         }
         getPlayers()
     }, [])
 
     return (
         <>
-            {players.map((player, index) => {
-                <p key={index}>{player.firstName}</p>
-            })}
+            <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+                {players.map((player, index) => (
+                    <div key={index} className="carousel-item">
+                        <PlayerCard firstName={player.firstName} lastName={player.lastName} height={player.height}
+                        position={player.position} PTS={player.PTS} REB={player.REB} AST={player.AST} btnTxt="Add to Board"
+                        srcImage="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                        altTxt="player name"/>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
